a037b80861514b9f890cc9f12ace5c8984fcf248
Asterios Raptis <asterios.raptis@gmx.net>
2018-03-10 17:02:21 +0100

    new project created for evaluate java objects

---
+++ jobject-evaluate/src/test/java/de/alpharogroup/evaluate/object/ComparableEvaluatorTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package de.alpharogroup.evaluate.object;
+
+import org.testng.annotations.Test;
+
+/**
+ * The class {@link ComparableEvaluator}.
+ */
+public class ComparableEvaluatorTest
+{
+
+	/**
+	 * Test method for {@link ComparableEvaluator#evaluateConsistency(Comparable, Comparable)}.
+	 */
+	@Test(enabled = false)
+	public void testEvaluateConsistency() throws Exception
+	{
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link ComparableEvaluator#evaluateReversalComparison(Comparable, Comparable)}.
+	 */
+	@Test(enabled = false)
+	public void testEvaluateReversalComparison() throws Exception
+	{
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link ComparableEvaluator#evaluateTransitivity(Comparable, Comparable, Comparable)}.
+	 */
+	@Test(enabled = false)
+	public void testEvaluateTransitivity() throws Exception
+	{
+		throw new RuntimeException("not yet implemented");
+	}
+
+}

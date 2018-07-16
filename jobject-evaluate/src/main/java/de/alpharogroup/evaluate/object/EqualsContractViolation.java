1e84297ba20be881a73152f8e276853cf5a67a1b
Asterios Raptis <asterios.raptis@gmx.net>
2018-07-16 12:07:06 +0200

    new enums created that can represent contract violations

---
+++ jobject-evaluate/src/main/java/de/alpharogroup/evaluate/object/EqualsContractViolation.java
@@ -0,0 +1,9 @@
+package de.alpharogroup.evaluate.object;
+
+/**
+ * The enum {@link EqualsContractViolation} represents a contract violation as the name let presume
+ */
+public enum EqualsContractViolation
+{
+	REFLEXIVITY, SYMMETRICITY, TRANSITIVITY, NON_NULL, CONSISTENCY;
+}
---
+++ jobject-evaluate/src/main/java/de/alpharogroup/evaluate/object/HashcodeContractViolation.java
@@ -0,0 +1,9 @@
+package de.alpharogroup.evaluate.object;
+
+/**
+ * The enum {@link HashcodeContractViolation} represents a contract violation as the name let presume
+ */
+public enum HashcodeContractViolation
+{
+	CONSISTENCY, NULL_ARGUMENT, EQAUALITY, UNEQAUALITY, UNEQAUALITY_ERROR;
+}

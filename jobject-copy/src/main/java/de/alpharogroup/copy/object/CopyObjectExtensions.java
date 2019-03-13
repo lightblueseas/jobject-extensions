a35954c75e2771ae13838a305a710b5a345f1262
James Phelps <tyler.durden69@gmx.de>
2019-03-13 15:32:10 +0100

    Update CopyObjectExtensions.java

--- jobject-copy/src/main/java/de/alpharogroup/copy/object/CopyObjectExtensions.java
+++ jobject-copy/src/main/java/de/alpharogroup/copy/object/CopyObjectExtensions.java
@@ -60,8 +60,8 @@ public final class CopyObjectExtensions
      *                                <li>has no nullary constructor</li>
      *                                </ul>
      */
-    public static Object copyObject(@NonNull Object original) throws IllegalAccessException, InstantiationException {
-        Object destination = original.getClass().newInstance();
+    public static<T> T copyObject(@NonNull T original) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        T destination = (T) original.getClass().newInstance();
         return copyObject(original, destination);
     }
 	
@@ -85,7 +85,7 @@ public final class CopyObjectExtensions
      *                                <li>has no nullary constructor</li>
      *                                </ul>
      */
-    public static <ORIGINAL, DESTINATION> DESTINATION copyObject(@NonNull ORIGINAL original, @NonNull DESTINATION destination) throws IllegalAccessException, InstantiationException {
+    public static <ORIGINAL, DESTINATION> DESTINATION copyObject(@NonNull ORIGINAL original, @NonNull DESTINATION destination) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         for (Field field : original.getClass().getDeclaredFields()) {
             if (copyField(field, original, destination)) continue;
         }
@@ -114,12 +114,16 @@ public final class CopyObjectExtensions
      *             <li>has no nullary constructor</li>
      *             </ul>
      */
-    public static <ORIGINAL, DESTINATION> boolean copyField(@NonNull Field field, @NonNull ORIGINAL original, @NonNull DESTINATION destination) throws IllegalAccessException, InstantiationException {
+     public static <ORIGINAL, DESTINATION> boolean copyField(@NonNull Field field, @NonNull ORIGINAL original, @NonNull DESTINATION destination) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         field.setAccessible(true);
         if (field.get(original) == null || Modifier.isFinal(field.getModifiers())) {
             return true;
         }
-        if (field.getType().isPrimitive() || field.getType().equals(String.class)
+		if (field.getType().isEnum()) {
+			Enum o = (Enum) field.get(original);
+			String name = o.name();
+			field.set(destination, Enum.valueOf(field.getType().asSubclass(Enum.class), name));
+		} else if (field.getType().isPrimitive() || field.getType().equals(String.class)
                 || field.getType().getSuperclass().equals(Number.class)
                 || field.getType().equals(Boolean.class)) {
             field.set(destination, field.get(original));

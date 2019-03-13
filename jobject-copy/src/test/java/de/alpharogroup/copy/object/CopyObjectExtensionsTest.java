cb1a123ef719bcc93f4dbbb91f40a5729e92daae
James Phelps <tyler.durden69@gmx.de>
2019-03-13 15:34:00 +0100

    Update CopyObjectExtensionsTest.java

--- jobject-copy/src/test/java/de/alpharogroup/copy/object/CopyObjectExtensionsTest.java
+++ jobject-copy/src/test/java/de/alpharogroup/copy/object/CopyObjectExtensionsTest.java
@@ -43,6 +43,36 @@ public class CopyObjectExtensionsTest
 {
 
 	/**
+	 * Test method for {@link CopyObjectExtensions#copyObject(Object)}
+	 *
+	 * @throws IllegalAccessException
+	 *             if the caller does not have access to the property accessor method
+	 * @throws InvocationTargetException
+	 *             if the property accessor method throws an exception
+	 * @throws InstantiationException
+	 *             Thrown if one of the following reasons: the class object
+	 *             <ul>
+	 *             <li>represents an abstract class</li>
+	 *             <li>represents an interface</li>
+	 *             <li>represents an array class</li>
+	 *             <li>represents a primitive type</li>
+	 *             <li>represents {@code void}</li>
+	 *             <li>has no nullary constructor</li>
+	 *             </ul>
+	 */
+	@Test
+	public void testCopyObject()
+			throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException, InvocationTargetException
+	{
+		Person actual;
+		Person expected;
+
+		expected = Person.builder().gender(Gender.MALE).name("asterix").build();
+		actual = CopyObjectExtensions.copyObject(expected);
+		assertEquals(expected, actual);
+	}
+
+	/**
 	 * Test method for {@link CopyObjectExtensions#copy(Object, Object)}.
 	 *
 	 * @throws IllegalAccessException
